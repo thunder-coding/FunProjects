c02b0f1bb90ebd647f9b339a6f090236384d2889
YAKSH BARIYA <yakshbari4@gmail.com>
2020-09-24 22:26:31 +0530

    Update Tictactoe.py

--- Tictactoe.py
+++ Tictactoe.py
@@ -77,6 +77,8 @@ while 1:
             x = input("Would you like to play again?? Y/N :  ")
             if x == "y":
                 player = X
+                board[0]=board[1]=board[2]=board[3]=board[4]=board[5]=board[6]=board[7]=board[8]=0
+                drawBoard()
                 break
             elif x == "n":
                 sys.exit("Player did not want to play again")
